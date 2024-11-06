54d4980cb95761d88dd538acca1e2c5b64def41d
Fernando Ismael Navarrete Ruiz <fernando.navarrete@toka.mx>
2024-11-06 15:03:17 -0600

    Created screen for park form

--- app/src/main/java/com/example/practica09_almacenamientoconsqlite/activities/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/practica09_almacenamientoconsqlite/activities/ui/home/HomeFragment.kt
@@ -4,37 +4,66 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.practica09_almacenamientoconsqlite.databinding.FragmentHomeBinding
+import com.example.practica09_almacenamientoconsqlite.databinding.FragmentParkFormBinding
+import com.google.android.material.textfield.TextInputEditText
+import org.w3c.dom.Text
 
 class HomeFragment : Fragment() {
 
-private var _binding: FragmentHomeBinding? = null
-  // This property is only valid between onCreateView and
-  // onDestroyView.
-  private val binding get() = _binding!!
+    private var _binding: FragmentParkFormBinding? = null
 
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View {
-    val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
 
-    _binding = FragmentHomeBinding.inflate(inflater, container, false)
-    val root: View = binding.root
+    private lateinit var textViewTitle: TextView
+    private lateinit var parkNameInput: TextInputEditText
+    private lateinit var parkSizeInput: TextInputEditText
+    private lateinit var parkLongitudeInput: TextInputEditText
+    private lateinit var parkLatitudeInput: TextInputEditText
+    private lateinit var registerParkButton: Button
+    private lateinit var findParkButton: Button
+    private lateinit var editParkButton: Button
+    private lateinit var deleteParkButton: Button
 
-    val textView: TextView = binding.textHome
-    homeViewModel.text.observe(viewLifecycleOwner) {
-      textView.text = it
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val homeViewModel =
+            ViewModelProvider(this)[HomeViewModel::class.java]
+
+        _binding = FragmentParkFormBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        textViewTitle = binding.parkFormTitle;
+        parkNameInput = binding.parkNameInput
+        parkSizeInput = binding.parkSizeInput
+        parkLongitudeInput = binding.parkLocationLongitudeInput
+        parkLatitudeInput = binding.parkLocationLatitudeInput
+        registerParkButton = binding.registerParkButton
+        findParkButton = binding.findParkButton
+        editParkButton = binding.editParkButton
+        deleteParkButton = binding.deleteParkButton
+
+        deleteParkButton.setOnClickListener {
+            Toast.makeText(activity, "Hello world", Toast.LENGTH_SHORT).show()
+        }
+
+        homeViewModel.text.observe(viewLifecycleOwner) {
+            //textView.text = it
+        }
+        return root
     }
-    return root
-  }
 
-override fun onDestroyView() {
+    override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
---
+++ app/src/main/java/com/example/practica09_almacenamientoconsqlite/models/park.kt
@@ -0,0 +1,3 @@
+package com.example.practica09_almacenamientoconsqlite.models
+
+class park(var parkName: String, val id: Int, var longitude: Double, var latitude: Double, var size: Double)
